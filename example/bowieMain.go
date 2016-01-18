e589966a194c2a294e4239d3915fe8b12fbbe937
dwenske <daniel.wenske@gmail.com>
2016-01-18 17:52:43 +0100

    Added basic data for held datatype & some data files. Also started reading/writing json

--- example/bowieMain.go
+++ example/bowieMain.go
@@ -1,30 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "github.com/Schokomuesl1/bowie"
+	"encoding/json"
+	"fmt"
+	"github.com/Schokomuesl1/bowie"
+	"io/ioutil"
 )
 
-func main() {
-    //KL := bowie.MakeEigenschaft("KL", 12)
-    MU := bowie.MakeEigenschaft("MU", 12)
-    GE := bowie.MakeEigenschaft("GE", 12)
-    KK := bowie.MakeEigenschaft("KK", 12)
-    //FF := bowie.MakeEigenschaft("FF", 12)
-    //IN := bowie.MakeEigenschaft("IN", 12)
-    //CH := bowie.MakeEigenschaft("CH", 12)
-    t := bowie.MakeTalent("Selbstbeherrschung", 5, MU, KK, GE)
-    fmt.Println(t)
-    MU.Increment()
-    fmt.Println(t)
-    ini := bowie.MakeCalculatedDependentValue("Initiative", 0.5, []bowie.DependentValue{MU, GE})
-    fmt.Println(ini)
-    success, ergebnis := KK.Probe()
+type EigenschaftEntry struct {
+	Kurz string
+	Lang string
+}
 
-    for i := -5; i < 6; i++ {
-        success, ergebnis := KK.ProbeMod(i)
-        fmt.Println(success, ergebnis)
-    }
-    fmt.Println("Probe: KK: ", KK.Value(), success, ergebnis)
-    fmt.Println(t.Probe())
+func main() {
+	//KL := bowie.MakeEigenschaft("KL", 12)
+	/*MU := bowie.MakeEigenschaft("MU", 12)
+	GE := bowie.MakeEigenschaft("GE", 12)
+	KK := bowie.MakeEigenschaft("KK", 12)
+	//FF := bowie.MakeEigenschaft("FF", 12)
+	//IN := bowie.MakeEigenschaft("IN", 12)
+	//CH := bowie.MakeEigenschaft("CH", 12)
+	t := bowie.MakeTalent("Selbstbeherrschung", 5, MU, KK, GE)
+	fmt.Println(t)
+	MU.Increment()
+	fmt.Println(t)
+	ini := bowie.MakeCalculatedDependentValue("Initiative", 0.5, []bowie.DependentValue{MU, GE})
+	fmt.Println(ini)
+	success, ergebnis := KK.Probe()
+
+	for i := -5; i < 6; i++ {
+		success, ergebnis := KK.ProbeMod(i)
+		fmt.Println(success, ergebnis)
+	}
+	fmt.Println("Probe: KK: ", KK.Value(), success, ergebnis)
+	fmt.Println(t.Probe())*/
+	h := bowie.NewHeld()
+	h.Eigenschaften.Set("MU", 8)
+	//h.Eigenschaften.Set("KL", 9)
+	//h.Eigenschaften.Set("GE", 10)
+	//h.Eigenschaften.Set("KK", 11)
+	//h.Eigenschaften.Set("FF", 12)
+	//h.Eigenschaften.Set("IN", 13)
+	h.Eigenschaften.Set("CH", 14)
+	//h.Eigenschaften.Set("KO", 15)
+	//h.NewTalent("Selbstbeherrschung", [3]string{"MU", "KK", "KO"})
+	fmt.Print(h)
+	h.Eigenschaften.Set("KO", 15)
+	fmt.Print(h) /*file, _ := ioutil.ReadFile("data/eigenschaften.json")
+	//tmp := make([]EigenschaftEntry, 0)
+	tmp := make(map[string]string)
+	json.Unmarshal([]byte(string(file)), &tmp)
+	fmt.Print(tmp)*/
+	file, _ := ioutil.ReadFile("data/talente.json")
+	AlleTalente := make([]bowie.TalentParse, 0)
+	json.Unmarshal([]byte(string(file)), &AlleTalente)
+	for _, v := range AlleTalente {
+		fmt.Println(v)
+		h.NewTalent(v.Name, v.Probe)
+	}
+	fmt.Print(h)
 }
---
+++ held.go
@@ -0,0 +1,127 @@
+package bowie
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+var AlleEigenschaften map[string]string
+
+func init() {
+	file, _ := ioutil.ReadFile("data/eigenschaften.json")
+	AlleEigenschaften = make(map[string]string)
+	json.Unmarshal([]byte(string(file)), &AlleEigenschaften)
+}
+
+type modPair struct {
+	Id    string
+	Value int32
+}
+type KulturType struct {
+	Name          string
+	Modifications []modPair
+}
+
+type EigenschaftHandler struct {
+	Eigenschaften map[string]*Eigenschaft
+}
+
+func (e *EigenschaftHandler) Set(name string, value int) bool {
+	if !e.exists(name) {
+		e.Add(name)
+	}
+	e.Eigenschaften[name].Set(value)
+	return true
+}
+
+func (e *EigenschaftHandler) exists(name string) bool {
+	_, existing := e.Eigenschaften[name]
+	return existing
+}
+
+func NewEigenschaftHandler() *EigenschaftHandler {
+	return &EigenschaftHandler{Eigenschaften: make(map[string]*Eigenschaft)}
+}
+
+func (e *EigenschaftHandler) Add(name string) bool {
+	if e.exists(name) {
+		return false
+	}
+	e.Eigenschaften[name] = MakeEigenschaft(name, 0)
+	return true
+}
+
+type TalentHandler struct {
+	Talente map[string]*Talent
+}
+
+func NewTalentHandler() *TalentHandler {
+	return &TalentHandler{Talente: make(map[string]*Talent)}
+}
+
+func (t *TalentHandler) Exists(name string) bool {
+	_, existing := t.Talente[name]
+	return existing
+}
+
+func (t *TalentHandler) Add(talent *Talent) bool {
+	if t.Exists(talent.Name) {
+		return false
+	}
+	t.Talente[talent.Name] = talent
+	return true
+}
+
+type Held struct {
+	Name          string
+	Kultur        KulturType
+	Eigenschaften EigenschaftHandler
+	Talente       TalentHandler
+}
+
+func NewHeld() *Held {
+	h := Held{Eigenschaften: *NewEigenschaftHandler(), Talente: *NewTalentHandler()}
+	for k, _ := range AlleEigenschaften {
+		h.Eigenschaften.Add(k)
+	}
+	return &h
+}
+
+//String prints an overview of the hero
+func (h *Held) String() string {
+	ret := fmt.Sprintf("Name: %s, Kultur: %s\n", h.Name, h.Kultur.Name)
+	ret += "Eigenschaften\n"
+	ret += "-------------\n"
+	for _, v := range h.Eigenschaften.Eigenschaften {
+		ret += fmt.Sprintf("%s: %d\n", v.Name, v.Value())
+	}
+	ret += "Talente\n"
+	ret += "-------\n"
+	for _, v := range h.Talente.Talente {
+		ret += fmt.Sprintf("%s\n", v.String())
+	}
+	return ret
+}
+
+func (h *Held) NewTalent(name string, eigenschaften [3]string) bool {
+	if h.Talente.Exists(name) {
+		return false
+	}
+	e1 := h.Eigenschaften.Eigenschaften[eigenschaften[0]]
+	e2 := h.Eigenschaften.Eigenschaften[eigenschaften[1]]
+	e3 := h.Eigenschaften.Eigenschaften[eigenschaften[2]]
+	h.Talente.Add(MakeTalent(name, 0, e1, e2, e3))
+	if h.Talente.Exists(name) {
+		return true
+	}
+	return false
+}
+
+type TalentParse struct {
+	Name      string
+	Kategorie string
+	Probe     [3]string
+	Belastung string
+	Kosten    string
+}
