ac57d868abee3862176be4c1dc133c251afad774
Daniel Wenske <daniel.wenske@comyno.com>
2016-01-18 21:08:42 +0100

    Berechne Grundwerte

--- example/bowieMain.go
+++ example/bowieMain.go
@@ -1,69 +1,57 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/Schokomuesl1/bowie"
-	"io/ioutil"
+    //"encoding/json"
+    "fmt"
+    "github.com/Schokomuesl1/bowie"
+    //"io/ioutil"
 )
 
 type EigenschaftEntry struct {
-	Kurz string
-	Lang string
+    Kurz string
+    Lang string
 }
 
 func main() {
-	//KL := bowie.MakeEigenschaft("KL", 12)
-	/*MU := bowie.MakeEigenschaft("MU", 12)
-	GE := bowie.MakeEigenschaft("GE", 12)
-	KK := bowie.MakeEigenschaft("KK", 12)
-	//FF := bowie.MakeEigenschaft("FF", 12)
-	//IN := bowie.MakeEigenschaft("IN", 12)
-	//CH := bowie.MakeEigenschaft("CH", 12)
-	t := bowie.MakeTalent("Selbstbeherrschung", 5, MU, KK, GE)
-	fmt.Println(t)
-	MU.Increment()
-	fmt.Println(t)
-	ini := bowie.MakeCalculatedDependentValue("Initiative", 0.5, []bowie.DependentValue{MU, GE})
-	fmt.Println(ini)
-	success, ergebnis := KK.Probe()
+    //KL := bowie.MakeEigenschaft("KL", 12)
+    /*MU := bowie.MakeEigenschaft("MU", 12)
+        GE := bowie.MakeEigenschaft("GE", 12)
+        KK := bowie.MakeEigenschaft("KK", 12)
+        //FF := bowie.MakeEigenschaft("FF", 12)
+        //IN := bowie.MakeEigenschaft("IN", 12)
+        //CH := bowie.MakeEigenschaft("CH", 12)
+        t := bowie.MakeTalent("Selbstbeherrschung", 5, MU, KK, GE)
+        fmt.Println(t)
+        MU.Increment()
+        fmt.Println(t)
+        ini := bowie.MakeCalculatedDependentValue("Initiative", 0.5, []bowie.DependentValue{MU, GE})
+        fmt.Println(ini)
+        success, ergebnis := KK.Probe()
 
-	for i := -5; i < 6; i++ {
-		success, ergebnis := KK.ProbeMod(i)
-		fmt.Println(success, ergebnis)
-	}
-	fmt.Println("Probe: KK: ", KK.Value(), success, ergebnis)
-	fmt.Println(t.Probe())*/
-	h := bowie.NewHeld()
-	h.Eigenschaften.Set("MU", 8)
-	//h.Eigenschaften.Set("KL", 9)
-	//h.Eigenschaften.Set("GE", 10)
-	//h.Eigenschaften.Set("KK", 11)
-	//h.Eigenschaften.Set("FF", 12)
-	//h.Eigenschaften.Set("IN", 13)
-	h.Eigenschaften.Set("CH", 14)
-	//h.Eigenschaften.Set("KO", 15)
-	//h.NewTalent("Selbstbeherrschung", [3]string{"MU", "KK", "KO"})
-	fmt.Print(h)
-	h.Eigenschaften.Set("KO", 15)
-	fmt.Print(h) /*file, _ := ioutil.ReadFile("data/eigenschaften.json")
-	//tmp := make([]EigenschaftEntry, 0)
-	tmp := make(map[string]string)
-	json.Unmarshal([]byte(string(file)), &tmp)
-	fmt.Print(tmp)*/
-	file, _ := ioutil.ReadFile("data/talente.json")
-	AlleTalente := make([]bowie.TalentParse, 0)
-	json.Unmarshal([]byte(string(file)), &AlleTalente)
-	for _, v := range AlleTalente {
-		fmt.Println(v)
-		h.NewTalent(v.Name, v.Probe)
-	}
-	fmt.Print(h)
+        for i := -5; i < 6; i++ {
+      	success, ergebnis := KK.ProbeMod(i)
+      	fmt.Println(success, ergebnis)
+        }
+        fmt.Println("Probe: KK: ", KK.Value(), success, ergebnis)
+        fmt.Println(t.Probe())*/
 
-	file2, _ := ioutil.ReadFile("data/spezies.json")
-	AlleSpezies := make([]bowie.Spezies, 0)
-	json.Unmarshal([]byte(string(file2)), &AlleSpezies)
-	for _, v := range AlleSpezies {
-		fmt.Println(v)
-	}
+    h := bowie.NewHeld()
+    h.Eigenschaften.Set("MU", 8)
+    h.Eigenschaften.Set("KL", 9)
+    h.Eigenschaften.Set("GE", 10)
+    h.Eigenschaften.Set("KK", 11)
+    h.Eigenschaften.Set("FF", 12)
+    h.Eigenschaften.Set("IN", 13)
+    h.Eigenschaften.Set("CH", 14)
+    h.Eigenschaften.Set("KO", 15)
+    h.SetSpezies("Mensch")
+    //h.NewTalent("Selbstbeherrschung", [3]string{"MU", "KK", "KO"})
+    fmt.Print(h)
+    h.Eigenschaften.Set("KO", 15)
+    fmt.Print(h) /*file, _ := ioutil.ReadFile("data/eigenschaften.json")
+      //tmp := make([]EigenschaftEntry, 0)
+      tmp := make(map[string]string)
+      json.Unmarshal([]byte(string(file)), &tmp)
+      fmt.Print(tmp)*/
+    fmt.Print(h)
 }
--- held.go
+++ held.go
@@ -1,144 +1,276 @@
 package bowie
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "io/ioutil"
 )
 
 var AlleEigenschaften map[string]string
+var AlleSpezies map[string]SpeziesType
+var AlleTalente []TalentType
 
 func init() {
-	file, _ := ioutil.ReadFile("data/eigenschaften.json")
-	AlleEigenschaften = make(map[string]string)
-	json.Unmarshal([]byte(string(file)), &AlleEigenschaften)
+    file, _ := ioutil.ReadFile("data/eigenschaften.json")
+    AlleEigenschaften = make(map[string]string)
+    json.Unmarshal([]byte(string(file)), &AlleEigenschaften)
+
+    file2, _ := ioutil.ReadFile("data/spezies.json")
+    speziesTmp := make([]SpeziesType, 0)
+    json.Unmarshal([]byte(string(file2)), &speziesTmp)
+    AlleSpezies = make(map[string]SpeziesType)
+    for _, v := range speziesTmp {
+        AlleSpezies[v.Name] = v
+    }
+    file3, _ := ioutil.ReadFile("data/talente.json")
+    AlleTalente = make([]TalentType, 0)
+    json.Unmarshal([]byte(string(file3)), &AlleTalente)
 }
 
 type modPair struct {
-	Id    string
-	Value int32
+    Id    string
+    Value int32
 }
 type KulturType struct {
-	Name          string
-	Modifications []modPair
+    Name          string
+    Modifications []modPair
 }
 
 type EigenschaftHandler struct {
-	Eigenschaften map[string]*Eigenschaft
+    Eigenschaften map[string]*Eigenschaft
 }
 
 func (e *EigenschaftHandler) Set(name string, value int) bool {
-	if !e.exists(name) {
-		e.Add(name)
-	}
-	e.Eigenschaften[name].Set(value)
-	return true
+    if !e.Exists(name) {
+        e.Add(name)
+    }
+    e.Eigenschaften[name].Set(value)
+    return true
 }
 
-func (e *EigenschaftHandler) exists(name string) bool {
-	_, existing := e.Eigenschaften[name]
-	return existing
+func (e *EigenschaftHandler) Exists(name string) bool {
+    _, existing := e.Eigenschaften[name]
+    return existing
 }
 
 func NewEigenschaftHandler() *EigenschaftHandler {
-	return &EigenschaftHandler{Eigenschaften: make(map[string]*Eigenschaft)}
+    return &EigenschaftHandler{Eigenschaften: make(map[string]*Eigenschaft)}
 }
 
 func (e *EigenschaftHandler) Add(name string) bool {
-	if e.exists(name) {
-		return false
-	}
-	e.Eigenschaften[name] = MakeEigenschaft(name, 0)
-	return true
+    if e.Exists(name) {
+        return false
+    }
+    e.Eigenschaften[name] = MakeEigenschaft(name, 0)
+    return true
+}
+
+func (e *EigenschaftHandler) Get(name string) *Eigenschaft {
+    if !e.Exists(name) {
+        return nil
+    }
+    return e.Eigenschaften[name]
 }
 
 type TalentHandler struct {
-	Talente map[string]*Talent
+    Talente map[string]*Talent
 }
 
 func NewTalentHandler() *TalentHandler {
-	return &TalentHandler{Talente: make(map[string]*Talent)}
+    return &TalentHandler{Talente: make(map[string]*Talent)}
 }
 
 func (t *TalentHandler) Exists(name string) bool {
-	_, existing := t.Talente[name]
-	return existing
+    _, existing := t.Talente[name]
+    return existing
 }
 
 func (t *TalentHandler) Add(talent *Talent) bool {
-	if t.Exists(talent.Name) {
-		return false
-	}
-	t.Talente[talent.Name] = talent
-	return true
+    if t.Exists(talent.Name) {
+        return false
+    }
+    t.Talente[talent.Name] = talent
+    return true
+}
+
+//func MakeCalculatedDependentValue(name string, mult float32, base []DependentValue) *CalculatedDependentValue {
+
+type Basiswert struct {
+    Grundwert     int
+    Berechnung    *CalculatedDependentValue
+    Modifikatoren []int // werden gesamt addiert/subtrahiert
+}
+
+func MakeBasiswert(name string, mult float32, base []DependentValue, grundwert int) *Basiswert {
+    return &Basiswert{Grundwert: grundwert, Berechnung: MakeCalculatedDependentValue(name, mult, base), Modifikatoren: make([]int, 0)}
+}
+
+func (b *Basiswert) Value() int {
+    tmp := b.Grundwert + b.Berechnung.Value()
+    for _, v := range b.Modifikatoren {
+        tmp += v
+    }
+    return tmp
+}
+
+func (b *Basiswert) AddModifier(value int) {
+    n := len(b.Modifikatoren)
+    if n == cap(b.Modifikatoren) {
+        newMod := make([]int, len(b.Modifikatoren), 2*len(b.Modifikatoren)+1)
+        copy(newMod, b.Modifikatoren)
+        b.Modifikatoren = newMod
+    }
+    b.Modifikatoren = b.Modifikatoren[0 : n+1]
+    b.Modifikatoren[n] = value
+}
+
+type BerechneteWerte struct {
+    Lebensenergie   *Basiswert // Grundwert + 2*KO + Vor/Nachteile
+    Astralenergie   *Basiswert // Grundwert + Leiteigenschaft + Vor/Nachteile
+    Karmaenergie    *Basiswert // Grundwert + Leiteigenschaft + Vor/Nachteile
+    Seelenkraft     *Basiswert // Grundwert + (MU + KL + IN)/6 + Vor/Nachteile
+    Zaehigkeit      *Basiswert // Grundwert + (KO + KO + KK)/6 + Vor/Nachteile
+    Ausweichen      *Basiswert // GE / 2
+    Initiative      *Basiswert // (MU + GE)/2 + Vor/Nachteile
+    Geschwindigkeit *Basiswert // Grundwert + Vor/Nachteile)
+}
+
+func InitBerechneteWerte(spezies *SpeziesType, eigenschaften *EigenschaftHandler) *BerechneteWerte {
+    return &BerechneteWerte{
+        Lebensenergie:   MakeBasiswert("Lebensenergie", 2, []DependentValue{eigenschaften.Get("KO")}, spezies.LE),
+        Astralenergie:   nil, // check this
+        Karmaenergie:    nil, // check this
+        Seelenkraft:     MakeBasiswert("Seelenkraft", (1.0 / 6.0), []DependentValue{eigenschaften.Get("MU"), eigenschaften.Get("KL"), eigenschaften.Get("IN")}, spezies.SK),
+        Zaehigkeit:      MakeBasiswert("Zaehigkeit", (1.0 / 6.0), []DependentValue{eigenschaften.Get("KO"), eigenschaften.Get("KO"), eigenschaften.Get("KK")}, spezies.ZK),
+        Ausweichen:      MakeBasiswert("Ausweichen", 0.5, []DependentValue{eigenschaften.Get("GE")}, 0),
+        Initiative:      MakeBasiswert("Initiative", 0.5, []DependentValue{eigenschaften.Get("GE"), eigenschaften.Get("MU")}, 0),
+        Geschwindigkeit: MakeBasiswert("Geschwindigkeit", 1, []DependentValue{}, spezies.GS)}
 }
 
 type Held struct {
-	Name          string
-	Spezies       string
-	Kultur        KulturType
-	Eigenschaften EigenschaftHandler
-	Talente       TalentHandler
+    Name          string
+    Spezies       SpeziesType
+    Basiswerte    BerechneteWerte
+    Kultur        KulturType
+    Eigenschaften EigenschaftHandler
+    Talente       TalentHandler
 }
 
 func NewHeld() *Held {
-	h := Held{Eigenschaften: *NewEigenschaftHandler(), Talente: *NewTalentHandler()}
-	for k, _ := range AlleEigenschaften {
-		h.Eigenschaften.Add(k)
-	}
-	return &h
+    h := Held{Eigenschaften: *NewEigenschaftHandler(), Talente: *NewTalentHandler()}
+    for k, _ := range AlleEigenschaften {
+        h.Eigenschaften.Add(k)
+    }
+    for _, v := range AlleTalente {
+        h.NewTalent(v.Name, v.Probe)
+    }
+    return &h
+}
+
+func (h *Held) SetSpezies(spezies string) error {
+    _, existing := AlleSpezies[spezies]
+    if !existing {
+        return errors.New("Spezies unbekannt!")
+    }
+    h.Spezies = AlleSpezies[spezies]
+    h.Basiswerte = *InitBerechneteWerte(&h.Spezies, &h.Eigenschaften)
+    return nil
 }
 
 //String prints an overview of the hero
 func (h *Held) String() string {
-	ret := fmt.Sprintf("Name: %s, Kultur: %s\n", h.Name, h.Kultur.Name)
-	ret += "Eigenschaften\n"
-	ret += "-------------\n"
-	for _, v := range h.Eigenschaften.Eigenschaften {
-		ret += fmt.Sprintf("%s: %d\n", v.Name, v.Value())
-	}
-	ret += "Talente\n"
-	ret += "-------\n"
-	for _, v := range h.Talente.Talente {
-		ret += fmt.Sprintf("%s\n", v.String())
-	}
-	return ret
+    ret := fmt.Sprintf("Name: %s, Kultur: %s\n", h.Name, h.Kultur.Name)
+    ret += "Eigenschaften\n"
+    ret += "-------------\n"
+    for _, v := range h.Eigenschaften.Eigenschaften {
+        ret += fmt.Sprintf("%s: %d\n", v.Name, v.Value())
+    }
+    ret += "Basiswerte\n"
+    ret += "----------\n"
+    if h.Basiswerte.Lebensenergie != nil {
+        ret += fmt.Sprintf("Lebensenergie: %d\n", h.Basiswerte.Lebensenergie.Value())
+    } else {
+        ret += "Lebensenergie: -\n"
+    }
+    if h.Basiswerte.Astralenergie != nil {
+        ret += fmt.Sprintf("Astralenergie: %d\n", h.Basiswerte.Astralenergie.Value())
+    } else {
+        ret += "Astralenergie: -\n"
+    }
+    if h.Basiswerte.Karmaenergie != nil {
+        ret += fmt.Sprintf("Karmaenergie: %d\n", h.Basiswerte.Karmaenergie.Value())
+    } else {
+        ret += "Karmaenergie: -\n"
+    }
+    if h.Basiswerte.Seelenkraft != nil {
+        ret += fmt.Sprintf("Seelenkraft: %d\n", h.Basiswerte.Seelenkraft.Value())
+    } else {
+        ret += "Seelenkraft: -\n"
+    }
+    if h.Basiswerte.Zaehigkeit != nil {
+        ret += fmt.Sprintf("Zaehigkeit: %d\n", h.Basiswerte.Zaehigkeit.Value())
+    } else {
+        ret += "Zaehigkeit: -\n"
+    }
+    if h.Basiswerte.Ausweichen != nil {
+        ret += fmt.Sprintf("Ausweichen: %d\n", h.Basiswerte.Ausweichen.Value())
+    } else {
+        ret += "Ausweichen: -\n"
+    }
+    if h.Basiswerte.Initiative != nil {
+        ret += fmt.Sprintf("Initiative: %d\n", h.Basiswerte.Initiative.Value())
+    } else {
+        ret += "Initiative: -\n"
+    }
+    if h.Basiswerte.Geschwindigkeit != nil {
+        ret += fmt.Sprintf("Geschwindigkeit: %d\n", h.Basiswerte.Geschwindigkeit.Value())
+    } else {
+        ret += "Geschwindigkeit: -\n"
+    }
+    ret += "Talente\n"
+    ret += "-------\n"
+    for _, v := range h.Talente.Talente {
+        ret += fmt.Sprintf("%s\n", v.String())
+    }
+    return ret
 }
 
 func (h *Held) NewTalent(name string, eigenschaften [3]string) bool {
-	if h.Talente.Exists(name) {
-		return false
-	}
-	e1 := h.Eigenschaften.Eigenschaften[eigenschaften[0]]
-	e2 := h.Eigenschaften.Eigenschaften[eigenschaften[1]]
-	e3 := h.Eigenschaften.Eigenschaften[eigenschaften[2]]
-	h.Talente.Add(MakeTalent(name, 0, e1, e2, e3))
-	if h.Talente.Exists(name) {
-		return true
-	}
-	return false
+    if h.Talente.Exists(name) {
+        return false
+    }
+    e1 := h.Eigenschaften.Eigenschaften[eigenschaften[0]]
+    e2 := h.Eigenschaften.Eigenschaften[eigenschaften[1]]
+    e3 := h.Eigenschaften.Eigenschaften[eigenschaften[2]]
+    h.Talente.Add(MakeTalent(name, 0, e1, e2, e3))
+    if h.Talente.Exists(name) {
+        return true
+    }
+    return false
 }
 
-type TalentParse struct {
-	Name      string
-	Kategorie string
-	Probe     [3]string
-	Belastung string
-	Kosten    string
+type TalentType struct {
+    Name      string
+    Kategorie string
+    Probe     [3]string
+    Belastung string
+    Kosten    string
 }
 
 type EigenschaftenModSpezies struct {
-	Eigenschaft []string
-	Mod         int
+    Eigenschaft []string
+    Mod         int
 }
 
-type Spezies struct {
-	Name                       string
-	LE                         int
-	SK                         int
-	GS                         int
-	EigenschaftsModifikationen []EigenschaftenModSpezies
-	Vorteile                   []string
-	Nachteile                  []string
-	AP                         int
+type SpeziesType struct {
+    Name                       string
+    LE                         int
+    SK                         int
+    ZK                         int
+    GS                         int
+    EigenschaftsModifikationen []EigenschaftenModSpezies
+    Vorteile                   []string
+    Nachteile                  []string
+    AP                         int
 }
